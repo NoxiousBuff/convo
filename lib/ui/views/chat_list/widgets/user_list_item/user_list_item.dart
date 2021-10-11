c7a5b10ecb8aef1dbab7d1e683bc8d8ff65740ed
NoxiousBuff <vikas9680523@gmail.com>
2021-10-11 22:55:20 +0530

    animating the loading builder

--- lib/ui/views/chat_list/widgets/user_list_item/user_list_item.dart
+++ lib/ui/views/chat_list/widgets/user_list_item/user_list_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hint/constants/app_keys.dart';
 import 'package:hint/models/user_model.dart';
 import 'package:hint/services/chat_service.dart';
+import 'package:hint/ui/shared/ui_helpers.dart';
 import 'package:hint/ui/views/chat_list/widgets/user_item.dart';
 
 class UserListItem extends StatelessWidget {
@@ -13,22 +14,92 @@ class UserListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
-      future: FirebaseFirestore.instance
-        .collection(subsFirestoreKey)
-        .doc(userUid)
-        .get(),
-      builder: (context, snapshot) {
-        if(!snapshot.hasData) {
-          return const CircularProgressIndicator.adaptive();
-        }
-        if(snapshot.hasError) {
-          return const Center(child: Text('It has Error'),);
-        }
-        FireUser fireUser = FireUser.fromFirestore(snapshot.data!);
-        return UserItem(fireUser: fireUser, onTap: () {
-          chatService.startConversation(context, fireUser, Color(fireUser.colorHexCode));
+        future: FirebaseFirestore.instance
+            .collection(subsFirestoreKey)
+            .doc(userUid)
+            .get(),
+        builder: (context, snapshot) {
+          Widget child;
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('It has Error'),
+            );
+          }
+          if (!snapshot.hasData) {
+            child = loadingUserListItem(context);
+          } else {
+            FireUser fireUser = FireUser.fromFirestore(snapshot.data!);
+            child = UserItem(
+                fireUser: fireUser,
+                onTap: () {
+                  chatService.startConversation(
+                      context, fireUser, Color(fireUser.colorHexCode));
+                });
+          }
+
+          return AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: child,
+          );
         });
-    });
+  }
+
+  Widget loadingUserListItem(BuildContext context) {
+    return ListTile(
+      onTap: () {},
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          bottomLeft: Radius.circular(20),
+        ),
+      ),
+      leading: ClipOval(
+        child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: [
+                Colors.indigo.shade300.withAlpha(30),
+                Colors.indigo.shade400.withAlpha(50),
+              ],
+              focal: Alignment.topLeft,
+              radius: 0.8,
+            ),
+          ),
+          height: 56.0,
+          width: 56.0,
+          child: const Text(
+            '',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+      title: Padding(
+        padding: const EdgeInsets.only(bottom: 5.0),
+        child: Container(
+          margin: EdgeInsets.only(
+              right: screenWidthPercentage(context, percentage: 0.1)),
+          decoration: BoxDecoration(
+            color: Colors.indigo.shade400.withAlpha(30),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Text(''),
+        ),
+      ),
+      subtitle: Container(
+        margin: EdgeInsets.only(
+            right: screenWidthPercentage(context, percentage: 0.4)),
+        decoration: BoxDecoration(
+          color: Colors.indigo.shade300.withAlpha(30),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: const Text(''),
+      ),
+    );
   }
 
   // Widget buildUserListItem (BuildContext context, UserListItemModel model {
