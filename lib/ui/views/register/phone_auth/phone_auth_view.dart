358d5caa41ccb4d199f719c81247a3f1f6ce254c
crankyvein <crankyvein@gmail.com>
2021-10-18 01:35:43 +0530

    phone auth view

---
+++ lib/ui/views/register/phone_auth/phone_auth_view.dart
@@ -0,0 +1,51 @@
+import 'package:stacked/stacked.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:hint/ui/views/register/phone_auth/phone_auth_viewmodel.dart';
+
+class PhoneAuthView extends StatelessWidget {
+  const PhoneAuthView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<PhoneAuthViewModel>.reactive(
+      viewModelBuilder: () => PhoneAuthViewModel(),
+      builder: (context, model, child) => Scaffold(
+        appBar: const CupertinoNavigationBar(
+          middle: Text('PhoneAuth'),
+        ),
+        body: SizedBox(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              const SizedBox(height: 50),
+              if (model.phoneAuthCredential != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(model.phoneAuthCredential!.providerId),
+                ),
+              const Divider(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: model.phoneTech,
+                ),
+              ),
+              const Divider(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OutlinedButton(
+                    onPressed: () => model.signUpWithPhone(),
+                    child: const Text('Sign Up')),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
