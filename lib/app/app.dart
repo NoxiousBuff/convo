9be1f6fbcc0cabb6f8aa3c07172a3082a6ad1b76
NoxiousBuff <vikas9680523@gmail.com>
2021-10-22 19:53:41 +0530

    state bugs fixed

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hint/constants/app_keys.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hint/prototypes/phone/phone_auth_view.dart';
 import 'package:hint/ui/views/chat_list/chat_list_view.dart';
 import 'package:hint/ui/views/register/email/email_register_view.dart';
 
@@ -12,16 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.transparent, statusBarColor: Colors.transparent));
+        systemNavigationBarColor: Colors.transparent,
+        statusBarColor: Colors.transparent));
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        restorationScopeId: appName,
-        title: appName,
-        theme: ThemeData(),
-        home: FirebaseAuth.instance.currentUser != null
-            ?  ChatListView()
-            : const PhoneAuthView(),
-      );
+      debugShowCheckedModeBanner: false,
+      restorationScopeId: appName,
+      title: appName,
+      theme: ThemeData(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.userChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            FirebaseAuth.instance.currentUser!.reload();
+            return ChatListView(); 
+          } else {
+            return const EmailRegisterView();
+          }
+        },
+      ),
+    );
   }
 }
